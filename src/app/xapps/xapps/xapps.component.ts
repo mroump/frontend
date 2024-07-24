55d44dfd1f322dc96099a4d53947892fb2f7045d
mroumpa <mr#XIA!r>
2024-07-24 15:39:06 +0300

    xApps

--- src/app/xapps/xapps/xapps.component.ts
+++ src/app/xapps/xapps/xapps.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
-import { XappsService } from '../xapps.service';
-import { Xapp } from '../xapp';
 
 @Component({
   selector: 'app-xapps',
@@ -12,28 +10,6 @@ import { Xapp } from '../xapp';
   styleUrl: './xapps.component.css'
 })
 export class XappsComponent {
-  xapps: Xapp[] = [];
-
-  constructor(public xappsService: XappsService, 
-              private router: Router) { }
-
-  ngOnInit(): void {
-    this.xappsService.getAll().subscribe((data: Xapp[])=>{
-      this.xapps = data; 
-      console.log(this.xapps);
-    })  
-  }
-
-  filterxApps(filter: string) {
-    this.xappsService.filterxApps(filter).subscribe((filtereddata: Xapp[])=>{
-      this.xapps = filtereddata;
-    })   
-  }
-
-  deleteXapp(id:number) {
-    this.xappsService.deleteXapp(id).subscribe(res => {
-      location. reload();
-    })
-  }
+  
 
 }
